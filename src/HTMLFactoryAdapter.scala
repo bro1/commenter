033be70e3eb3b80b8d0055762acf8bffd3b900f4
Linas Jakucionis <linasj@gmail.com>
2009-09-14 12:55:01 +1200

    html parsing

---
+++ src/HTMLFactoryAdapter.scala
@@ -0,0 +1,10 @@
+import _root_.scala.xml.parsing.FactoryAdapter
+
+trait HTMLFactoryAdapter extends FactoryAdapter {
+
+  val emptyElements = Set("area", "base", "br", "col", "hr", "img",
+                          "input", "link", "meta", "param")
+
+  def nodeContainsText(localName: String) =
+    !(emptyElements contains localName)
+}
