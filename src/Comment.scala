b8615e718070074d87d0f414924f575d1d3fc58d
Linas Jakucionis <linasj@gmail.com>
2009-09-14 23:31:00 +1200

    Retrieval of comment data

---
+++ src/Comment.scala
@@ -0,0 +1,7 @@
+abstract class Comment {
+
+  var postedAd : java.util.Date = null
+  var postedBy : String
+  var text : String;  
+  
+}
--- src/Launcher.scala
+++ src/Launcher.scala
@@ -1,9 +1,64 @@
+import java.io.{File,FileReader}
+import java.text.{SimpleDateFormat}
+import java.util.Date
+
+import java.util.TimeZone
+  
 object Launcher {
-
+    
    def main(args: Array[String]) {
-      println("Huh?")
-    }
+      
+     val url = new FileReader (new java.io.File ("misc/examples/delfi/str1p1.html"))
+     val zz = new TagSoupFactoryAdapter load url
 
+     val comments = (zz \\ "div").filter( _.attribute("class").mkString == "comm-container")
+         
+     comments foreach {(com) =>
+//       println (com)
+       //println ((((com \ "comm-name")\"strong").text))
+cmt (com)       
+       
+     }
+      
+    
+      
+   }
+   
+   
+   def cmt (com : scala.xml.Node) {
+       println ("Nuo: " + 
+         ((com \ "div").filter(_.attribute("class").mkString == "comm-name") \ "strong").text)
+     
+       dt(com)
+       
+       val c =  (com \ "div").filter(
+         _.attribute("class").mkString == "comm-text")       
+       
+       val alerts = (c.first \ "div").filter(_.attribute("class").mkString != "comm-alerts")
+       
+       println (alerts)            
+     
+   }
+   
+   
+   def dt(com : scala.xml.Node)  {
+     
+     val strDate =            
+         ((com \ "div").filter(_.attribute("class").mkString == "comm-name") \ "div").text
+     
+       val df = new SimpleDateFormat("yyyy MM dd HH:mm");       
+       
+       
+       df.setTimeZone(java.util.TimeZone.getTimeZone("Europe/Vilnius"))
+       val d = df.parse(strDate)
+
+       
+       println(d)       
+       //2009 09 13 10:48
+     
+   }
+   
+   
   
   
 }
