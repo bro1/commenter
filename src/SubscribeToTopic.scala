56e0453cc15fcbdd4823c74d48227e7c8e905187
Linas Jakucionis <linasj@gmail.com>
2009-09-27 11:02:48 +1300

    read stuff from db

--- src/SubscribeToTopic.scala
+++ src/SubscribeToTopic.scala
@@ -24,18 +24,18 @@ object SubscribeToTopicWindow extends Frame {
     
   }
   
+  object urlField extends TextField {columns = 20}  
   
   contents = new GridBagPanel {
     
-    object urlField extends TextField {columns = 20}  
+  
     
     layout(new Label("Vardas: ")) = new Constraints {gridx = 0; gridy = 0}
     layout(urlField) = new Constraints {gridx = 1; gridy = 0; }
     layout(new Label("Topic Name: ")) = new Constraints {gridx = 0; gridy = 2; gridwidth = 2; fill = GridBagPanel.Fill.Both }
     layout(new Label("Topic Type: ")) = new Constraints {gridx = 0; gridy = 3; gridwidth = 2; fill = GridBagPanel.Fill.Both }
     
-    layout(buttonPannel) = new Constraints {gridx = 0; gridy = 4; gridwidth = 2; anchor = GridBagPanel.Anchor.CENTER }
-    
+    layout(buttonPannel) = new Constraints {gridx = 0; gridy = 4; gridwidth = 2; anchor = GridBagPanel.Anchor.CENTER }    
     
   }
   
@@ -44,17 +44,34 @@ object SubscribeToTopicWindow extends Frame {
   
   reactions += {
     case ButtonClicked(`buttonCancel`) => {
-      SubscribeToTopicWindow.visible = false;
+      SubscribeToTopicWindow.visible = false      
     }
+    
+   case ButtonClicked(`buttonSubscribe`) => {
+      subscribe(urlField.text)
+      SubscribeToTopicWindow.visible = false      
+    }
+    
+    case EditDone(`urlField`) => {
+      println("Edit done") 
+    }
+    
+  }
+  
+  
+  def subscribe(url : String) {
+    dbctest.subscribe(url, "", url)
   }
    
 } 
 
 
+
+
 object SubscribeToTopic {
   
   def main(args: Array[String]) {
-    dbctest
+    dbctest getSubscribtions
   }
 
   
@@ -87,17 +104,49 @@ object typ {
   
 }
 
+
 object dbctest  {
+  
+  val db = new Database(SqliteVendor)
   
   def subscribe(name : String, topicType : String, url : String) = { 
   
-    val db = new Database(SqliteVendor)
-     
     val data = InsertionData.Constructor(
       Some(List("name", "topictype", "url")), 
       List(typ char name , typ char topicType , typ char url ))
    
-    db executeStatement  Insert ("topic", data)
-  
+    db executeStatement  Insert ("topic", data)  
   }
+  
+  object t extends scala.dbc.statement.Table {
+    def tableName = "topic" 
+    
+    def tableRename = None
+    
+    def fieldTypes = List(DataTypeUtil.integer, DataTypeUtil.characterVarying(255), DataTypeUtil.characterVarying(255), DataTypeUtil.characterVarying(255))
+  }
+  
+  
+  object sel extends scala.dbc.statement.Select {
+    def havingClause = None
+    def groupByClause = None
+    def whereClause = None
+    def fromClause = List(t)
+    
+    
+    def selectList = List()
+    
+    def setQuantifier = None
+    
+    def fieldTypes = List(DataTypeUtil.integer, DataTypeUtil.characterVarying(255), DataTypeUtil.characterVarying(255), DataTypeUtil.characterVarying(255))
+  }
+  
+  
+  def getSubscribtions() = {    
+    
+    db executeStatement (sel)
+  }
+  
+  
+  
 }
